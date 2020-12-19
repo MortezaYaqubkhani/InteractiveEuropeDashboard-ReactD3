20afd002d3d01955d248093f9a778f9094c19bfc
MortezaYaqubkhani <yaqubkhani.m@gmail.com>
2020-12-19 02:14:40 +0100

    this is the version after adding basic interaction abilities to maps

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import * as d3 from 'd3';
 
 class Map extends Component {
-  componentDidMount(error, info) {
-    const mapWidth = 500,
+  async componentDidMount(error, info) {
+
+    //map 
+    const mapWidth = 350,
       mapHeight = 400;
 
     const mapsvg = d3
@@ -19,28 +21,59 @@ class Map extends Component {
       .center([6.0, 52.5])
       .scale(10000)
       .translate([mapWidth / 2, mapHeight / 2]);
+//mouse events functions
+      const mouseOverHandler = (d, i) => {
+        console.log('mouseover')
+      }
+
+      const clickHandler = () => {
+        console.log('click')
+
+      }
+
+      const mouseOutHandler = () => {
+        console.log('mouseout')
+        
+      }
 
     const svgpath = d3.geoPath().projection(myProj);
     // asynchronously load geojson:
-    d3.json(
-      'data/overijssel.json',
-      function (geojson) {
-        mapsvg
-        .append('svg:g')
-          .selectAll('path')
-          .data(geojson.features)
-          .enter()
-          // for each d create an svgpath that uses the geoPath generator:
-          .append('svg: path')
-          .append('svg')
-          //   .attr('class', 'municipality')
-          .attr('d', svgpath)
-          .style('background-color', 'red')
-          .style('stroke', 'rgb(250, 200, 250)')
-          .style('stroke-width', 2);
-      } //end of callback function
+    const map = await d3.json(
+      'data/overijssel.json'
+      // function (geojson) {
+      //   console.log(123)
+      // mapsvg
+      // .append('svg:g')
+      //   .selectAll('path')
+      //   .data(geojson.features)
+      //   .enter()
+      //   // for each d create an svgpath that uses the geoPath generator:
+      //   .append('svg: path')
+      //   .append('svg')
+      //   //   .attr('class', 'municipality')
+      //   .attr('d', svgpath)
+      //   .style('background-color', 'red')
+      //   .style('stroke', 'rgb(250, 200, 250)')
+      //   .style('stroke-width', 2);
+      // } //end of callback function
     );
-    console.log(mapsvg)
+
+    mapsvg
+      .selectAll('path')
+      .data(map.features)
+      .enter()
+      // for each d create an svgpath that uses the geoPath generator:
+      .append('path')
+      //   .attr('class', 'municipality')
+      .attr('d', svgpath)
+      .style('fill', 'white')
+      .style('stroke', 'rgb(250, 200, 250)')
+      .style('stroke-width', 2)
+      //mouse events
+      .on('mouseover', mouseOverHandler)
+      .on('mouseout', mouseOutHandler)
+      .on('click', clickHandler);
+    console.log(map.features);
   }
 
   render() {
---
+++ src/component/mapFnc.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect} from 'react';
+
+const MapFunction = () => {
+    return ( 
+        <div>this is the function map</div>
+     );
+}
+ 
+export default MapFunction;
